98a83a039b52d8d3bb1ac3c2507503bb7441aa16
nahuelex <nausoft2@hotmail.com>
2020-07-19 10:22:34 -0300

    commit1

---
+++ main.js
@@ -0,0 +1,433 @@
+function check1() {
+    text1 = document.getElementById("txt1").value;
+    text2 = document.getElementById("txt2").value;
+    text3 = document.getElementById("txt3").value;
+    text4 = document.getElementById("txt4").value;
+    text5 = document.getElementById("txt5").value;
+    text6 = document.getElementById("txt6").value;
+    text7 = document.getElementById("txt7").value;
+
+
+    if(text1.trim() == "western")
+      txt1.style.color = "green";
+    else
+    txt1.style.color = "red";
+
+
+    if(text2.trim() == "science fiction film")
+            txt2.style.color = "green";
+    else
+      txt2.style.color = "red";
+
+
+    if(text3.trim() == "disaster film")
+    txt3.style.color="green";
+    else
+      txt3.style.color = "red";
+
+    
+    if(text4.trim() == "musical")
+    txt4.style.color="green";
+    else
+      txt4.style.color = "red";
+
+    if(text5.trim() == "documentary film")
+    txt5.style.color="green";
+    else
+      txt5.style.color = "red";
+
+    if(text6.trim() == "animated film")
+    txt6.style.color="green";
+    else
+      txt6.style.color = "red";
+
+    
+    if(text7.trim() == "action film" || text7 == "war film")
+    txt7.style.color="green";
+    else
+    txt7.style.color="red";
+}
+
+function limpiar() {
+  txt1.value = "";
+  txt1.style.color = "black"
+  txt1.focus();
+
+  txt2.value = "";
+  txt2.style.color = "black"
+  
+  txt3.value = "";
+  txt3.style.color = "black"
+
+  txt4.value = "";
+  txt4.style.color = "black"
+
+  
+  txt5.value = "";
+  txt5.style.color = "black"
+
+  
+  txt6.value = "";
+  txt6.style.color = "black"
+
+  
+  txt7.value = "";
+  txt7.style.color = "black"
+
+
+}
+
+function check2(){
+  var text1 = document.getElementById("dos_1").value;
+  var text2 = document.getElementById("dos_2").value;
+  var text3 = document.getElementById("dos_3").value;
+  var text4 = document.getElementById("dos_4").value;
+  var text5 = document.getElementById("dos_5").value;
+  var text6 = document.getElementById("dos_6").value;
+  var text7 = document.getElementById("dos_7").value;
+
+  if(text1.trim() == "western")
+    dos_1.style.color = "green";
+    else
+      dos_1.style.color = "red";
+
+  if(text2.trim() == "documentary film")
+    dos_2.style.color = "green";
+    else
+      dos_2.style.color = "red";
+  
+
+  if(text3.trim() == "science fiction film")
+  dos_3.style.color = "green";
+  else
+      dos_3.style.color = "red";
+
+
+ if(text4.trim() == "action film")
+    dos_4.style.color="green";
+  else
+    dos_4.style.color = "red";
+
+  if(text5.trim() == "comedy")
+    dos_5.style.color = "green";
+  else
+    dos_5.style.color = "red";
+
+  if(text6.trim() == "historical drama")
+   dos_6.style.color = "green";
+  else
+    dos_6.style.color = "red";
+
+  if(text7.trim() == "horror film")
+   dos_7.style.color = "green";
+   else
+    dos_7.style.color = "red";
+
+}
+
+function limpiar2(){
+
+  dos_1.value = "";
+  dos_1.style.color = "black"
+  dos_1.focus();
+
+  dos_2.value = "";
+  dos_2.style.color = "black"
+  
+  dos_3.value = "";
+  dos_3.style.color = "black"
+
+  dos_4.value = "";
+  dos_4.style.color = "black"
+
+  
+  dos_5.value = "";
+  dos_5.style.color = "black"
+
+  
+  dos_6.value = "";
+  dos_6.style.color = "black"
+
+  
+  dos_7.value = "";
+  dos_7.style.color = "black"
+}
+
+
+function check3(){
+  var text1 = document.getElementById("tres_1").value;
+  var text2 = document.getElementById("tres_2").value;
+  var text3 = document.getElementById("tres_3").value;
+  var text4 = document.getElementById("tres_4").value;
+  var text5 = document.getElementById("tres_5").value;
+  var text6 = document.getElementById("tres_6").value;
+  var text7 = document.getElementById("tres_7").value;
+  var text8 = document.getElementById("tres_8").value;
+ 
+
+  if(text1.trim() == "3")
+    tres_1.style.color = "green";
+    else
+      tres_1.style.color = "red";
+
+  if(text2.trim() == "6")
+    tres_2.style.color = "green";
+    else
+      tres_2.style.color = "red";
+  
+
+  if(text3.trim() == "4")
+  tres_3.style.color = "green";
+  else
+      tres_3.style.color = "red";
+
+
+ if(text4.trim() == "8")
+    tres_4.style.color="green";
+  else
+    tres_4.style.color = "red";
+
+  if(text5.trim() == "2")
+    tres_5.style.color = "green";
+  else
+    tres_5.style.color = "red";
+
+  if(text6.trim() == "5")
+   tres_6.style.color = "green";
+  else
+    tres_6.style.color = "red";
+
+  if(text7.trim() == "1")
+   tres_7.style.color = "green";
+   else
+    tres_7.style.color = "red";
+
+    if(text8.trim() == "7")
+     tres_8.style.color = "green";
+    else
+      tres_8.style.color = "red";
+
+}
+
+function limpiar3(){
+  tres_1.value="";
+  tres_1.style.color= "black";
+  tres_1.focus();
+
+  tres_2.value="";
+  tres_2.style.color= "black";
+
+  tres_3.value="";
+  tres_3.style.color= "black";
+
+  tres_4.value="";
+  tres_4.style.color= "black";
+
+  tres_5.value="";
+  tres_5.style.color= "black";
+
+  tres_6.value="";
+  tres_6.style.color= "black";
+
+  tres_7.value="";
+  tres_7.style.color= "black";
+
+  tres_8.value="";
+  tres_8.style.color= "black";
+}
+
+
+function check4()
+{
+  var text9 = document.getElementById("tres_9").value;
+  var text10 = document.getElementById("tres_10").value;
+  var text11 = document.getElementById("tres_11").value;
+  var text12 = document.getElementById("tres_12").value;
+  var text13 = document.getElementById("tres_13").value;
+  var text14 = document.getElementById("tres_14").value;
+  var text15 = document.getElementById("tres_15").value;
+  var text16 = document.getElementById("tres_16").value;
+  var text17 = document.getElementById("tres_17").value;
+  var text18 = document.getElementById("tres_18").value;
+  
+if(text9.trim() == "10")
+    tres_9.style.color = "green";
+    else
+      tres_9.style.color = "red";
+
+  if(text10.trim() == "8")
+    tres_10.style.color = "green";
+    else
+      tres_10.style.color = "red";
+  
+
+  if(text11.trim() == "1")
+  tres_11.style.color = "green";
+  else
+      tres_11.style.color = "red";
+
+
+ if(text12.trim() == "6")
+    tres_12.style.color="green";
+  else
+    tres_12.style.color = "red";
+
+  if(text13.trim() == "2")
+    tres_13.style.color = "green";
+  else
+    tres_13.style.color = "red";
+
+  if(text14.trim() == "")
+   tres_14.style.color = "green";
+  else
+    tres_14.style.color = "red";
+
+  if(text15.trim() == "7")
+   tres_15.style.color = "green";
+   else
+    tres_15.style.color = "red";
+
+    if(text16.trim() == "3")
+     tres_16.style.color = "green";
+    else
+      tres_16.style.color = "red";
+
+      if(text17.trim() == "9")
+     tres_17.style.color = "green";
+    else
+      tres_17.style.color = "red";
+
+      if(text18.trim() == "5")
+     tres_18.style.color = "green";
+    else
+      tres_18.style.color = "red";
+
+}
+
+function limpiar4(){
+  tres_9.value="";
+  tres_9.style.color="black";
+  tres_9.focus();
+
+  tres_10.value="";
+  tres_10.style.color="black";
+
+  tres_11.value="";
+  tres_11.style.color="black";
+
+  tres_12.value="";
+  tres_12.style.color="black";
+
+  tres_13.value="";
+  tres_13.style.color="black";
+
+  tres_14.value="";
+  tres_14.style.color="black";
+
+  tres_15.value="";
+  tres_15.style.color="black";
+  
+  tres_16.value="";
+  tres_16.style.color="black";
+
+  tres_17.value="";
+  tres_17.style.color="black";
+
+  tres_18.value="";
+  tres_18.style.color="black";
+}
+
+function check5(){
+  var p = document.getElementById("ej1");
+  var p2 = document.getElementById("ej2");
+  var p3 = document.getElementById("ej3");
+  var p4 = document.getElementById("ej4");
+  var p5 = document.getElementById("ej5");
+  var p6 = document.getElementById("ej6");
+  var p7 = document.getElementById("ej7");
+  var p8 = document.getElementById("ej8");
+  var p9 = document.getElementById("ej9");
+  var p10 = document.getElementById("ej10");
+  var p11 = document.getElementById("ej11");
+  var p12 = document.getElementById("ej12");
+  var p13 = document.getElementById("ej13");
+  var p14 = document.getElementById("ej14");
+
+  var radio = document.getElementsByName("1");
+  var radio2 = document.getElementsByName("2");
+  var radio3 = document.getElementsByName("3");
+  var radio4 = document.getElementsByName("4");
+  var radio5 = document.getElementsByName("5");
+  var radio6 = document.getElementsByName("6");
+  var radio7 = document.getElementsByName("7");
+
+  if(radio[1].checked)
+  p2.style.color = "green";
+  else if(radio[0].checked)
+  p.style.color="red";
+
+  if(radio2[0].checked)
+  p3.style.color="green";
+  else if(radio2[1].checked)
+  p4.style.color="red";
+
+  if(radio3[0].checked)
+  p5.style.color="green";
+  else if(radio3[1].checked)
+  p6.style.color="red";
+
+  if(radio4[1].checked)
+  p8.style.color="green";
+  else if(radio4[0].checked)
+  p7.style.color="red";
+
+  if(radio5[1].checked)
+  p10.style.color="green";
+  else if(radio5[0].checked)
+  p9.style.color="red";
+
+  if(radio6[1].checked)
+  p12.style.color = "green";
+  else if(radio6[0].checked)
+  p11.style.color="red";
+
+  if(radio7[0].checked)
+  p13.style.color ="green";
+  else if(radio7[1].checked)
+  p14.style.color="red";
+
+}
+function limpiar5(){
+
+document.getElementById("ej1").style.color = "black";
+
+  var p2 = document.getElementById("ej2").style.color = "black";
+  var p3 = document.getElementById("ej3").style.color = "black";
+  var p4 = document.getElementById("ej4").style.color = "black";
+  var p5 = document.getElementById("ej5").style.color = "black";
+  var p6 = document.getElementById("ej6").style.color = "black";
+  var p7 = document.getElementById("ej7").style.color = "black";
+  var p8 = document.getElementById("ej8").style.color = "black";
+  var p9 = document.getElementById("ej9").style.color = "black";
+  var p10 = document.getElementById("ej10").style.color = "black";
+  var p11 = document.getElementById("ej11").style.color = "black";
+  var p12 = document.getElementById("ej12").style.color = "black";
+  var p13 = document.getElementById("ej13").style.color = "black";
+  var p14 = document.getElementById("ej14").style.color = "black";
+
+  document.getElementsByName("1")[0].checked=false;
+  document.getElementsByName("1")[1].checked=false;
+  document.getElementsByName("2")[0].checked=false;
+  document.getElementsByName("2")[1].checked=false;
+  document.getElementsByName("3")[0].checked=false;
+  document.getElementsByName("3")[1].checked=false;
+  document.getElementsByName("4")[0].checked=false;
+  document.getElementsByName("4")[1].checked=false;
+  document.getElementsByName("5")[0].checked=false;
+  document.getElementsByName("5")[1].checked=false;
+  document.getElementsByName("6")[0].checked=false;
+  document.getElementsByName("6")[1].checked=false;
+  document.getElementsByName("7")[0].checked=false;
+  document.getElementsByName("7")[1].checked=false;
+
+}
